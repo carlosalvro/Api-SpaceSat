609dfc337362a74e7e5f79182d1879408e7aed11
Carlos <carlose.alvro@gmail.com>
2022-03-29 23:31:06 -0600

    Team agregado

--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,9 @@
 const { MissionSchema, Mission } = require("./mission.model");
+const { TeamSchema, Team } = require("./team.model");
 
 function setupModels(sequelize) {
   Mission.init(MissionSchema, Mission.config(sequelize));
+  Team.init(TeamSchema, Team.config(sequelize));
 }
 
 module.exports = setupModels;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const missionRouter = require("./mission.router");
+const teamRouter = require("./team.router");
 
 function routerApi(app) {
   const router = express.Router();
   app.use("/api/v1", router);
   router.use("/mission", missionRouter);
+  router.use("/team", teamRouter);
 }
 
 module.exports = routerApi;
